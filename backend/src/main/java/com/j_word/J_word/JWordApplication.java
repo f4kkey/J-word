5f8b69c025fb846a46b0a204c4693f56d66a484e
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-07-03 09:04:06 +0700

    init

---
+++ backend/src/main/java/com/j_word/J_word/JWordApplication.java
@@ -0,0 +1,13 @@
+package com.j_word.J_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JWordApplication.class, args);
+	}
+
+}
