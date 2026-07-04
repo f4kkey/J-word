408882d2c5183bc8c587976ff3d52317f6d55653
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-07-04 23:46:16 +0700

    signup logic

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useAuth } from '../context/AuthContextProvider'
 
 export default function Feed() {
+    const { user, logout } = useAuth();
     return (
         <div className='grid h-full gap-8 min-[1135px]:grid-cols-[14rem_1fr_20rem] min-[1135px]:items-start'>
             <header className='h-16 flex justify-end items-center gap-4 p-4 rounded-none bg-gray-200'>
-                <div>Hello user</div>
+                <div>Hello {user?.email}</div>
                 <span>|</span>
-                <button>Logout</button>
+                <button onClick={logout}>Logout</button>
             </header>
             <main>
                 <div className='hidden min-[1135px]:block bg-gray-200'></div>
--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, type SubmitEvent } from 'react'
 import Input from '../../components/auth/Input'
 import Button from '../../components/auth/Button'
 import Seperator from '../../components/auth/Seperator'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContextProvider'
 
 export default function Signup() {
 
     const [errorMessage, setErrorMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
+    const { signup } = useAuth();
+    const navigate = useNavigate();
 
+    const doSignup = async (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true)
+        const email = e.currentTarget.email.value
+        const password = e.currentTarget.password.value
+
+        try {
+            await signup(email, password);
+            navigate("/");
+        } catch (error) {
+            if (error instanceof Error) {
+                setErrorMessage(error.message)
+            } else {
+                setErrorMessage("An unknown error occurred")
+            }
+        }
+        finally {
+            setIsLoading(false)
+        }
+    };
     return (
         <div>
             <h1>Sign up for a J** opportunity</h1>
             <p>Don't be scare</p>
-            <form>
+            <form onSubmit={doSignup}>
                 <Input type='email' id='email' label='Email' onFocus={() => setErrorMessage("")} />
                 <Input type='password' id='password' label='Password' onFocus={() => setErrorMessage("")} />
 
                 {errorMessage && <p className='mb-4 text-red-600'>{errorMessage}</p>}
 
-                <Button type='submit'> Create Account</Button>
+                <Button type='submit' disabled={isLoading}>
+                    {isLoading ? "..." : "Create Account"}
+                </Button>
             </form>
             <Seperator>Or</Seperator>
             <div className='text-center'>Already on J-word?{" "}
